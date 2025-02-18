90fa0a5377cc49a52a9c2cc644bdc31aeb7a03dc
Davzzz2 <123270465+Davzzz2@users.noreply.github.com>
2025-02-18 15:00:47 +0100

    Add files via upload

---
+++ prags.js
@@ -0,0 +1,12 @@
+const slotData = [
+  {
+    "name": "Sweet Bonanza 1000",
+    "link": "https://stake.com/casino/games/pragmatic-play-sweet-bonanza-1000",
+    "image": "https://mediumrare.imgix.net/a77d914f12935f85ca574c3b52989b13cb48901ab8c55bdf326e1f2a177cbd97?w=180&h=236&fit=min&auto=format"
+  },
+  {
+    "name": "Gates of Olympus 1000",
+    "link": "https://stake.com/casino/games/pragmatic-play-gates-of-olympus-1000",
+    "image": "https://mediumrare.imgix.net/2a83b2bfe89f30834cda26834e0715500c2c2885024069091fe7964ac3fa8adf?w=180&h=236&fit=min&auto=format"
+  }
+]
